3e6052df3e8be372d4ee62bc10e237158bc7994d
thanhan123 <thanhan.uit@gmail.com>
2017-12-04 21:30:01 +0700

    remove unused code

--- FlickrBrowser/Scence/SceneCoordinator.swift
+++ FlickrBrowser/Scence/SceneCoordinator.swift
@@ -9,20 +9,6 @@
 import UIKit
 
 class SceneCoordinator: SceneCoordinatorType {
-//    private static func topViewController(_ base: UIViewController? = UIApplication.shared.keyWindow?.rootViewController) -> UIViewController? {
-//        if let nav = base as? UINavigationController {
-//            return topViewController(nav.visibleViewController)
-//        }
-//        if let tab = base as? UITabBarController {
-//            if let selected = tab.selectedViewController {
-//                return topViewController(selected)
-//            }
-//        }
-//        if let presented = base?.presentedViewController {
-//            return topViewController(presented)
-//        }
-//        return base
-//    }
     
     private var currVC: UIViewController
     private var window: UIWindow
